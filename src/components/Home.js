36bacb537baaabc07590030de14671a3f00db467
hukushpakush <sharmin.tamanna@lc.cuny.edu>
2020-12-18 18:24:17 -0500

    removed unused files and added react-dom

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Hi</h2>
+      <h2>I am Sharmin Tamanna</h2>
+      <h3>Software Engineer</h3>
+    </div>
+  );
+}
