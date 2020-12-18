b71a34740d980b5c3e93c66dddec621be80a8c4b
hukushpakush <sharmin.tamanna@lc.cuny.edu>
2020-12-18 18:37:00 -0500

    modified homepage and connected all the pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Home from "./components/Home";
+import "./index.css";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../css/Home.css";
+import me from "../images/me.png";
 
 export default function Home() {
   return (
     <div className="home">
-      <h2>Hi</h2>
-      <h2>I am Sharmin Tamanna</h2>
-      <h3>Software Engineer</h3>
+      
+      <h2>Hi! I am Sharmin Tamanna</h2>
+      <img src={me}alt ="picture of me"></img>
+      <h2></h2>
     </div>
   );
 }
